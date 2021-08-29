9a8eab579e391579cce0e01eda0b8ee2a5cba877
Jannik Arndt <jannik@jannikarndt.de>
2021-08-29 11:27:55 +0200

    Initial Commit

---
+++ Charts/ChartsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsApp.swift
+//  Charts
+//
+//  Created by Jannik Arndt on 29.08.21.
+//
+
+import SwiftUI
+
+@main
+struct ChartsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
